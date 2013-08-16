00948e2a9c83ad17613e6d2a9e22f8ecd0dea7ce
kumkee <kumkeejjl@gmail.com>
2013-08-16 11:00:07 +0800

    add leap month notion

--- src/chinese_cal_date.c
+++ src/chinese_cal_date.c
@@ -31,7 +31,7 @@ void handle_init(AppContextRef ctx) {
   text_layer_set_text_color(&text_cdate_layer, GColorWhite);
   text_layer_set_background_color(&text_cdate_layer, GColorClear);
   layer_set_frame(&text_cdate_layer.layer, GRect(8, 56, 128, 56));
-  text_layer_set_font(&text_cdate_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
+  text_layer_set_font(&text_cdate_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
   layer_add_child(&window.layer, &text_cdate_layer.layer);
 
 }
@@ -41,6 +41,7 @@ void GenerateCDateText(PblTm *t, char* cdtext, bool ZhChars)
 {
 
   Date today;
+  int i;
 
   today.year  = t->tm_year + 1900;
   today.month = t->tm_mon + 1;
@@ -48,18 +49,23 @@ void GenerateCDateText(PblTm *t, char* cdtext, bool ZhChars)
   today.hour  = t->tm_hour;
 
   Solar2Lunar(&today);
+  i = today.leap?1:0;
 
-  cdtext[0] = today.month/10 + 48;
-  cdtext[1] = today.month%10 + 48;
-  cdtext[3] = today.day/10 + 48;
-  cdtext[4] = today.day%10 + 48;
+  if(i)
+	{ cdtext[0] = 'L'; }
+  cdtext[i+0] = today.month/10 + '0';
+  cdtext[i+1] = today.month%10 + '0';
+  cdtext[i+2] = '|';
+  cdtext[i+3] = today.day/10 + '0';
+  cdtext[i+4] = today.day%10 + '0';
+  cdtext[i+5] = 0;
   
 }
 
 
 void handle_hour_tick(AppContextRef ctx, PebbleTickEvent *evt) {
 
-  static char ccd_text[] = "00|00";
+  static char ccd_text[] = "L00|00";
 
   GenerateCDateText(evt->tick_time, ccd_text, false);
 
